536c959deccb59015e26d4abe407a06f4adafc03
chungalung <aawchung@gmail.com>
2013-12-03 20:22:22 -0800

    testing

---
+++ questions.js
@@ -0,0 +1,72 @@
+var qs = [];
+
+qs['MountainView'] = {
+	q: "Which of the following companies has their HQ in Mountain View?",
+	choices: ["Google","Texas Instruments","SAP","Toyota"],
+	answer: 0,
+	score: 10
+  };
+ 
+qs['Sacramento'] = {
+	q: "Sacramento is the birthplace of what historic mailing service?",
+	choices: ["UPS","Pony Express","DHL", "FedEx"],
+	answer: 1,
+	score: 15
+  };
+   
+qs['SanDiego'] = {
+	q: "San Diego is next to which country?",
+	choices: ["Mexico","Canada","Hawaii", "South Africa"],
+	answer: 0,
+	score: 5
+  };	
+   
+qs['Modesto'] = {
+	q: "Of following famous actors, which one is from Modesto?",
+	choices: ["George Lucas","Bill Cosby","Jessica Simpson","Homer Simpson"],
+	answer: 0,
+	score: 15
+  };
+  
+qs['Tahoe'] = {
+	q: "What popular winter activity is Lake Tahoe known for?",
+	choices: ["Tetris","Drag Racing","Planking","Skiing"],
+	answer: 3,
+	score: 5
+  };	
+     
+qs['LasVegas'] = {
+	q: "Which Cirque du Soleil show is an MGM Grand exclusive?",
+	choices: ["Zumanity","Totem","The Phantom of the Opera","Kà"],
+	answer: 3,
+	score: 15
+  };	
+   
+qs['Reno'] = {
+	q: "Which of the following is a Reno Hotel?",
+	choices: ["Bellagio","Silver Legacy","Wynn", "Caesars Palace"],
+	answer: 1,
+	score: 15
+  };	
+   
+qs['LosAngeles'] = {
+	q: "Which of the following cities is located on the beachfront?",
+	choices: ["Thousand Oaks","Beverly Hills","Santa Monica", "Monterey Park"],
+	answer: 2,
+	score: 10
+  };	
+   
+qs['SanFrancisco'] = {
+	q: "Which famous actor played as the main character in the Dirty Harry movie series filmed in San Francisco.",
+	choices: ["Clint Eastwood","Tom Cruise","Sylvester Stallone", "Terry Cruz"],
+	answer: 0,
+	score: 10
+  };	
+   
+qs['SantaBarbara'] = {
+	q: "What famous public university is located in Santa Barbara?",
+	choices: ["CalTech","DeVry","UC Santa Barbara","UC Irvine"],
+	answer: 2,
+	score: 5
+  };	
+  
